5c1da8382a3c1208652ef720a60e47c9fdead4f2
Anshumaan Chaturvedi <anshumaan20191@hotmail.com>
2021-04-20 13:41:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,20 @@
     {
         static void Main(string[] args)
         {
-            const string XML_FILE_PATH = "D:\\Practice-Dev\\UCC_Coding_Exam\\sampleXML\\SampleXML.xml";
-            XMLFileReader reader = new XMLFileReader();
-            reader.ReturnListOFGrantors(XML_FILE_PATH);
-            reader.WarnTheUserForPastRecordDate(XML_FILE_PATH);
-            reader.Update_Required_Attribute_Of_Beneficiary_LastName(XML_FILE_PATH);
+           Console.WriteLine("Please enter the full path of file with extension at end:");
+            string xmlFilePath = Console.ReadLine();
+            string isAnXMLFile = Path.GetExtension(xmlFilePath);
+            if(isAnXMLFile != null && isAnXMLFile.Contains(".xml"))
+            {
+                XMLFileReader reader = new XMLFileReader();
+                reader.ReturnListOfGrantors(xmlFilePath);
+                reader.WarnTheUserForPastRecordDate(xmlFilePath);
+                reader.Update_Required_Attribute_Of_Beneficiary_LastName(xmlFilePath);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid File! Please give a full path of an xml file only.");
+            }
         }
     }
 }
